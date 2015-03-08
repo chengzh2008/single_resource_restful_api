2dee7c37db9bbeeaaec9f0ffe853198017856b41
chengzh2008 <chengzh2008@gmail.com>
2015-03-07 16:56:04 -0800

    add angular blog controller and karma config file

--- app/js/blogs/controller/blogs_controler.js
+++ app/js/blogs/controller/blogs_controler.js
@@ -1,3 +1,65 @@
-/**
- * Created by xiaoyazi on 3/6/15.
- */
+'use strict';
+
+module.exports = function(app) {
+    app.constructor('notesController', ['$scope', '$http', function ($scope, $http) {
+        var blogsUrl = '/api/v1/blogs';
+        $scope.blogs = [];
+
+        $scope.getAll = function () {
+            $http({
+                method: 'GET',
+                url: blogsUrl
+            }).success(function (data, status) {
+                $scope.blogs = data;
+            }).error(function (data, status) {
+                console.log(data);
+            });
+        };
+
+        $scope.create = function(blog) {
+            $http({
+                method: 'POST',
+                url: blogsUrl,
+                data: blog
+            }).success(function(data){
+                $scope.blogs.push(data);
+            }).error(function (data) {
+                console.log(data);
+            });
+        };
+
+        $scope.save = function (blog) {
+            $http({
+                method: 'PUT',
+                url: blogsUrl + '/' + blog._id,
+                data: blog
+            }).success(function (data) {
+                blog.editing = false;
+            }).error(function (data) {
+                console.log(data);
+            });
+        };
+
+        $scope.remove = function (blog) {
+            $http({
+                method: 'DELETE',
+                url: blogsUrl + '/' + blog._id
+            }).success(function () {
+                $scope.blogs.splice($scope.blogs.indexOf(blog), 1);
+            }).error(function (data) {
+                console.log(data);
+            });
+        };
+
+        $scope.toggleEdit = function (blog) {
+            if(blog.editing) {
+                blog = blog.oldData;
+                blog.editing = false;
+            } else {
+                blog.oldData = blog;
+                blog.editing = true;
+            }
+        };
+
+    }]);
+};
